d6bf3cf15f6ec53e2f64d7ad67d1a20ea508fa15
hmitroglou <haralambos.mitroglou@gmx.de>
2021-11-17 21:22:33 +0100

    Test auf anderer Maschine

--- training_quickdraw/quickdraw_npy.py
+++ training_quickdraw/quickdraw_npy.py
@@ -7,7 +7,7 @@
 conda clean -tipsy
 
 
-# In[1]:
+# In[2]:
 
 
 import tensorflow as tf
@@ -16,8 +16,10 @@ import tensorflow as tf
 
 import numpy as np
 from numpy import asarray
+
 import matplotlib.pyplot as plt 
 from matplotlib import image
+
 import random as rd
 import math as m
 from PIL import Image
@@ -27,31 +29,24 @@ from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
 
 import requests as req
-#import npzviewer
-#import wget
-#from bs4 import BeautifulSoup
 import csv
-
 import os
 print(os.getcwd())
 
-#from google.cloud import storage
-#client = storage.Client()
 import copy
-#pip install --user google-cloud-storage
 
 print(tf.__version__)
 
 
-# In[2]:
+# In[1]:
 
 
-def download_data(categories,number_of_samples):
+def download_npy(categories,number_of_samples):
 
     number_of_categories = len(categories)
     data = np.array([], dtype=np.int64).reshape(0,784)
     for i in range(number_of_categories):
-        filename = cats[i][0] + '.npy'
+        filename = categories[i][0] + '.npy'
         filename = filename.replace(" ","%20")
         
         url = 'https://storage.googleapis.com/quickdraw_dataset/full/numpy_bitmap/'+filename
@@ -75,16 +70,6 @@ def load_data(name,n):
     return data[0:n,:]
 
 #randomize data and labels
-def randomize_data(data,labels,seed):
-    np.random.seed(seed)
-    np.random.shuffle(data)
-    np.random.seed(seed)
-    np.random.shuffle(cat_id)
-
-
-# In[3]:
-
-
 def shuff(data,label,seed):
     
     s = np.shape(data)
@@ -106,73 +91,87 @@ def shuff(data,label,seed):
     
     l1 = list(range(n))
     l2 = list(range(n))
-    random.seed(seed)
+    np.random.seed(seed)
     np.random.shuffle(l1)
-    random.seed(seed)
+    np.random.seed(seed)
     np.random.shuffle(l2)   
     
     for i in range(n):
         data_new[i] = data[l1[i]]
         label_new[i] = int(label[l2[i]])
         
+    del data, label
     return data_new, label_new
 
+def download_and_save(number_of_categories,number_of_samples):
+
+    x = [random.randint(0, 345) for p in range(0, number_of_categories)]
+
+    #load categories
+    categories = open("categories.txt",'r')
+    reader = csv.reader(categories)
+    categories = [row for row in reader]
+    categories = [categories[row] for row in x]
     
+    d = download_npy(categories,number_of_samples)
+    
+    #save Data
+    filename = 'dataset/data_{}_{}.csv'.format(number_of_categories,number_of_samples)
+    np.savetxt(filename, d, delimiter=',')
+    
+    #save Categories
+    filename = 'dataset/cat_{}_{}.csv'.format(number_of_categories,number_of_samples)
+    with open(filename, 'w') as f: 
+        write = csv.writer(f) 
+        write.writerows(categories)
+    return d, categories
+    
+def data_from_file(number_of_categories,number_of_samples):
+    
+    filename = 'dataset/data_{}_{}.csv'.format(number_of_categories,number_of_samples)
+    d = np.loadtxt(filename, delimiter=',')
+    filename = 'dataset/cat_{}_{}.csv'.format(number_of_categories,number_of_samples)
+    categories = open(filename,'r')
+    reader = csv.reader(categories)
+    categories = [row for row in reader]
+    
+    return d, categories
 
 
-# In[6]:
+# In[9]:
 
 
-x = [random.randint(0, 345) for p in range(0, 10)]
+number_of_categories = 5
+number_of_samples = 100
 
-#load categories
-cats = open("categories.txt",'r')
-reader = csv.reader(cats)
-cats = [row for row in reader]
-cats = [cats[row] for row in x] #random categories
-
-number_of_categories = len(cats)
-number_of_samples = 1000
-
-#load data from google storage
-#d = download_data(cats[0:number_of_categories],number_of_samples)
-#filename = 'data_{}_{}.csv'.format(number_of_categories,number_of_samples)
-#np.savetxt(filename, d, delimiter=',')
-#filename = 'cat_{}_{}.csv'.format(number_of_categories,number_of_samples)
-#np.savetxt(filename, cats, delimiter=',')
-
-
-#load data from file
-filename = 'data_{}_{}.csv'.format(number_of_categories,number_of_samples)
-d = np.loadtxt(filename, delimiter=',')
-filename = 'cat_{}_{}.csv'.format(number_of_categories,number_of_samples)
-#cats = np.loadtxt(filename, delimiter=',')
+##load data
+#d, cats = download_and_save(number_of_categories,number_of_samples)
+d, cats = data_from_file(number_of_categories,number_of_samples)
 
 cat_id = np.repeat(range(number_of_categories),number_of_samples)+1
 
-#data = np.reshape(d,(len(d),28,28))
-
-#reshape data into 28x28
-#data = d.copy()
-#data = np.reshape(data,(len(d),28,28))
+print('Categories: ', cats)
+print(np.shape(d),' ',np.shape(cat_id))
 
 
-# In[7]:
-
-
-cats
-
-
-# In[8]:
+# In[21]:
 
 
 #Shuffle data
 data,cat_id = shuff(d,cat_id,13)
+
+#reshape data into 28x28
 data = np.reshape(data,(len(data),28,28))
 print(np.shape(data),' ',np.shape(cat_id))
 
 
-# In[9]:
+# In[18]:
+
+
+data = np.reshape(d,(len(d),28,28))
+
+
+# In[22]:
 
 
 #split data
@@ -185,11 +184,11 @@ y_train = cat_id[0:m.floor(training*len(cat_id))]
 x_test = data[m.ceil(training*len(data)):len(data)]/ 255.0
 y_test = cat_id[m.ceil(training*len(cat_id)):len(cat_id)]
 
-print('Train-Set: ','Samples',np.shape(x_train)[0], np.shape(y_train)[0])
-print('Test-Set: ','Samples',np.shape(x_test)[0], np.shape(y_test)[0])
+print('Train-Set: ','Samples',np.shape(x_train)[0],'/ Labels', np.shape(y_train)[0])
+print('Test-Set: ','Samples',np.shape(x_test)[0],'/ Labels', np.shape(y_test)[0])
 
 
-# In[10]:
+# In[23]:
 
 
 #TEST Show
@@ -205,7 +204,7 @@ model = tf.keras.models.Sequential([
   tf.keras.layers.Flatten(input_shape=(28, 28)),
   tf.keras.layers.Dense(128, activation='relu'),
   tf.keras.layers.Dropout(0.2),
-  tf.keras.layers.Dense(10),
+  tf.keras.layers.Dense(number_of_categories),
   tf.keras.layers.Softmax()
 ])
 
@@ -218,13 +217,13 @@ model.compile(optimizer='adam',
               metrics=['accuracy'])
 
 #train the model
-model.fit(x_train, y_train,batch_size = 50, epochs=10)
+model.fit(x_train, y_train,batch_size = 50, epochs=5)
 
 
-# In[11]:
+# In[44]:
 
 
-batch_size = 50
+batch_size = 100
 len(x_train)/batch_size
 
 
